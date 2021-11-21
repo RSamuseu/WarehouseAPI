9de68ac94d4b5fd066b68da564168d126a600e3e
Raman Samuseu <r.samuseu@godeltech.com>
2021-11-22 02:59:24 +0300

    added view models

---
+++ WarehouseAPI/WarehouseAPI.Web/ViewModels/CarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseAPI.Web.ViewModels
+{
+    public class CarViewModel
+    {
+        public string Location { get; set; }
+
+        public List<VehicleViewModel> Vehicles { get; set; }
+    }
+}
---
+++ WarehouseAPI/WarehouseAPI.Web/ViewModels/WarehouseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseAPI.Web.ViewModels
+{
+    public class WarehouseViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public LocationViewModel Location { get; set; }
+
+        public CarViewModel Cars { get; set; }
+    }
+}
