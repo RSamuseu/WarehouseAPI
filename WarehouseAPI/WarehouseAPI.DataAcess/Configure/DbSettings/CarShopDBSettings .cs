f5d1022920d476e86ad618a569bf2c97257ee7aa
Raman Samuseu <r.samuseu@godeltech.com>
2021-11-22 03:34:32 +0300

    interface IDBSettings and implementation

---
+++ WarehouseAPI/WarehouseAPI.DataAcess/Configure/DbSettings/CarShopDBSettings .cs
@@ -0,0 +1,8 @@
+﻿namespace WarehouseAPI.DataAccess.Configure.DbSettings
+{
+    public class CarShopDBSettings : IDBSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
