e39d03fa05435b1d36a38dae3fd88a76ab7e35a5
Raman Samuseu <r.samuseu@godeltech.com>
2021-11-22 03:17:19 +0300

    added file for reading datas from json file

---
+++ WarehouseAPI/WarehouseAPI.DataAcess/Initializer/DataInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WarehouseAPI.DataAcess.Entities;
+
+namespace WarehouseAPI.DataAccess.Initializer
+{
+    public static class DataInitializer
+    {
+        public static List<Warehouse> DataList()
+        {
+            JArray jsonArray = JArray.Parse(File.ReadAllText("warehouses.json"));
+            string output = JsonConvert.SerializeObject(jsonArray);
+            List<Warehouse> deserializedProduct = JsonConvert.DeserializeObject<List<Warehouse>>(output);
+            return deserializedProduct;
+        }
+    }
+}
