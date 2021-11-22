bec2e481b1152e8f54dba0fa468a793196f92539
Raman Samuseu <r.samuseu@godeltech.com>
2021-11-22 03:03:15 +0300

    added controller

--- WarehouseAPI/WarehouseAPI.Web/Controllers/CarController.cs
+++ WarehouseAPI/WarehouseAPI.Web/Controllers/CarController.cs
@@ -1,16 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WarehouseAPI.Business;
+using WarehouseAPI.Web.ViewModels;
 
-namespace Warehouse.Web.Controllers
+namespace WarehouseAPI.Web.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class CarController : Controller
     {
-        public IActionResult Index()
+        private readonly IWarehouseService _carService;
+        private readonly IMapper _mapper;
+
+        public CarController(IWarehouseService carService, IMapper mapper)
         {
-            return View();
+            _carService = carService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var cars = await _carService.GetAllItems();
+            
+            if(cars is null)
+            {
+                return NotFound();
+            }
+
+            var items = _mapper.Map<List<WarehouseViewModel>>(cars);
+            return Ok(items);
         }
     }
 }
