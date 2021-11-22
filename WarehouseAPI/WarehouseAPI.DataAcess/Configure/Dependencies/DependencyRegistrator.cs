73ae9525500367a90e2999eecb3285699a8eeabf
Raman Samuseu <r.samuseu@godeltech.com>
2021-11-22 03:34:14 +0300

    moved file to another folder

---
+++ WarehouseAPI/WarehouseAPI.DataAcess/Configure/Dependencies/DependencyRegistrator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using WarehouseAPI.DataAccess.Configure.DbSettings;
+
+namespace WarehouseAPI.DataAcess.Configure.Dependencies
+{
+    public static class DependencyRegistrator
+    {
+        public static IServiceCollection AddDataAccessComponents(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IDBSettings>(options =>
+                options.GetRequiredService<IOptions<CarShopDBSettings>>().Value);
+            return services;
+        }
+    }
+}
