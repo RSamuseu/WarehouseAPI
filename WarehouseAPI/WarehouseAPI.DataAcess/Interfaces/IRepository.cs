92c6b023ffdb782f8f65f9d8033d32979ce174d9
Raman Samuseu <r.samuseu@godeltech.com>
2021-11-22 03:35:07 +0300

    added and implemented repository

---
+++ WarehouseAPI/WarehouseAPI.DataAcess/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WarehouseAPI.DataAccess.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAll();
+    }
+}
---
+++ WarehouseAPI/WarehouseAPI.DataAcess/Repositories/CarShopRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using WarehouseAPI.DataAccess.Configure.DbSettings;
+using WarehouseAPI.DataAccess.Initializer;
+using WarehouseAPI.DataAccess.Interfaces;
+using WarehouseAPI.DataAcess.Entities;
+
+namespace WarehouseAPI.DataAccess.Repositories
+{
+    public class CarShopRepository : IRepository<Warehouse>
+    {
+        private readonly IMongoCollection<Warehouse> _cars;
+
+        public CarShopRepository(IDBSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _cars = database.GetCollection<Warehouse>("Warehouse");
+            var t = DataInitializer.DataList();
+            _cars.InsertManyAsync(t);
+        }
+
+        public async Task<List<Warehouse>> GetAll()
+        {
+            var tt = await _cars.Find(car => true).ToListAsync();
+            return await _cars.Find(car => true).ToListAsync();
+        }
+    }
+}
